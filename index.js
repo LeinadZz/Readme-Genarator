6f30c10aa81255ab2fa443a5f1fda20376102487
Daniel <leinad6ix.z@gmail.com>
2022-10-03 20:22:46 -0700

    fixed questions

--- index.js
+++ index.js
@@ -19,124 +19,68 @@ inquirer.prompt(
         },
         {
             type : 'input',
-            message : "What is your Project title?",
-            name : "title",
+            message : "Write a brief description of your project.",
+            name : "description",
             validate: (value) =>{
                 if(value) {
                     return true
                 } else {
-                    return ''
+                    return 'Please write a description'
                 }
             }
         },
         {
             type : 'input',
-            message : "What is your Project title?",
-            name : "title",
+            message : "Please add installation intructions",
+            name : "installation",
+        },
+        {
+            type : 'input',
+            message : "Did anyone contribute to this project? If yes, please enter names",
+            name : "contribution",
+        },
+        {
+            type : 'input',
+            message : "Please add any guidelines",
+            name : "guideline",
+        },
+        {
+            type : 'input',
+            message : "Please add test instructions",
+            name : "instruction",
+        },
+        {
+            type : 'list',
+            message : "What license did you use for this project?",
+            name : "license",
+            choices: [
+                'MIT License',
+                'ISC License',
+            ]
+        },
+        {
+            type : 'input',
+            message : "What is your GitHub username?",
+            name : "username",
             validate: (value) =>{
                 if(value) {
                     return true
                 } else {
-                    return ''
+                    return 'Please enter your username'
                 }
             }
         },
         {
             type : 'input',
-            message : "What is your Project title?",
-            name : "title",
+            message : "What is your email?",
+            name : "email",
             validate: (value) =>{
                 if(value) {
                     return true
                 } else {
-                    return ''
+                    return 'Please enter your email'
                 }
             }
-        },
-        {
-            type : 'input',
-            message : "What is your Project title?",
-            name : "title",
-            validate: (value) =>{
-                if(value) {
-                    return true
-                } else {
-                    return ''
-                }
-            }
-        },
-        {
-            type : 'input',
-            message : "What is your Project title?",
-            name : "title",
-            validate: (value) =>{
-                if(value) {
-                    return true
-                } else {
-                    return ''
-                }
-            }
-        },
-        {
-            type : 'input',
-            message : "What is your Project title?",
-            name : "title",
-            validate: (value) =>{
-                if(value) {
-                    return true
-                } else {
-                    return ''
-                }
-            }
-        },
-        {
-            type : 'input',
-            message : "What is your Project title?",
-            name : "title",
-            validate: (value) =>{
-                if(value) {
-                    return true
-                } else {
-                    return ''
-                }
-            }
-        },
-        {
-            type : 'input',
-            message : "What is your Project title?",
-            name : "title",
-            validate: (value) =>{
-                if(value) {
-                    return true
-                } else {
-                    return ''
-                }
-            }
-        },
-        {
-            type : 'input',
-            message : "What is your Project title?",
-            name : "title",
-            validate: (value) =>{
-                if(value) {
-                    return true
-                } else {
-                    return ''
-                }
-            }
-        },
-        {
-            type : 'input',
-            message : "What is your Project title?",
-            name : "title",
-            validate: (value) =>{
-                if(value) {
-                    return true
-                } else {
-                    return ''
-                }
-            }
-        },
-        
+        }, 
     ]
 )
